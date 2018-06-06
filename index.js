ad00557264719b74827d12f433485b2241e99e14
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2018-06-06 15:27:22 -0500

    More robust error handling

--- index.js
+++ index.js
@@ -24,6 +24,25 @@ exports.handler = async function(event, context, callback) {
   console.log("event", event);
   if (event.body !== null && event.body !== undefined) {
     let data = JSON.parse(event.body);
+    if (
+      data.from_name === "" ||
+      data.from_email === "" ||
+      data.message === "" ||
+      data.recaptcha === ""
+    ) {
+      callback(null, {
+        statusCode: 400,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          en: "Please make sure you have filled out all of the form fields.",
+          fr: "Veuillez vérifier que vous avez bien rempli tous les champs."
+        }),
+        isBase64Encoded: false
+      });
+    }
     const params = {
       Destination: {
         ToAddresses: [process.env.TO_EMAIL]
@@ -67,15 +86,54 @@ exports.handler = async function(event, context, callback) {
               "Access-Control-Allow-Origin": "*",
               "Content-Type": "application/json"
             },
-            body: JSON.stringify({ message: "Done" }),
+            body: JSON.stringify({
+              en: "Thank you! I'll get in touch with you as soon as possible.",
+              fr: "Merci ! Je vous répondrai au plus tôt."
+            }),
             isBase64Encoded: false
           })
         )
-        .catch(err => console.error(err, err.stack));
+        .catch(
+          callback(null, {
+            statusCode: 400,
+            headers: {
+              "Access-Control-Allow-Origin": "*",
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+              en: "There was an error sending the email. Please try again.",
+              fr:
+                "Il y a une erreur pendant l'envoi du message. Veuillez réessayer ultérieurement."
+            }),
+            isBase64Encoded: false
+          })
+        );
     } else {
-      console.error("Captcha could not be validated");
+      callback(null, {
+        statusCode: 401,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          en: "There was an issue with the reCaptcha. Please try again.",
+          fr: "Il y a une erreur avec le reCaptcha. Veuillez réessayer."
+        }),
+        isBase64Encoded: false
+      });
     }
   } else {
-    console.error("No JSON body in email");
+    callback(null, {
+      statusCode: 400,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        en: "Please make sure you have filled out all of the form fields.",
+        fr: "Veuillez vérifier que vous avez bien rempli tous les champs."
+      }),
+      isBase64Encoded: false
+    });
   }
 };
